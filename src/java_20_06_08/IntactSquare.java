f9073bc92cbe1281c9432381d5031b1e08336de7
leeyc924 <leeyc924@naver.com>
2020-06-08 08:54:19 +0900

    IntactSquare code

---
+++ src/java_20_06_08/IntactSquare.java
@@ -0,0 +1,19 @@
+package java_20_06_08;
+
+public class IntactSquare {
+	public static void main(String[] args) {
+		int w = 8;
+		int h = 12;
+		long answer = 0;
+		long small, big;
+		big = (long) (w >= h ? w : h);
+		small = (long) (w < h ? w : h);
+		while (small != 0) {
+			long nmg = big % small;
+			big = small;
+			small = nmg;
+		}
+		answer = ((long) w * (long) h) - ((long) w + (long) h - big);
+		System.out.println(answer);
+	}
+}
