b1f772af71648ab9a43a1f619db51e54c9b6d0c1
leeyc924 <leeyc924@naver.com>
2020-06-12 09:06:44 +0900

    Lotto code

---
+++ src/java_20_06_12/Lotto/Lotto6NumMain.java
@@ -0,0 +1,11 @@
+package java_20_06_12.Lotto;
+
+public class Lotto6NumMain {
+
+	public static void main(String[] args) {
+		Lotto6Num lot = new Lotto6Num();
+		lot.make();
+		lot.print();
+	}
+
+}
