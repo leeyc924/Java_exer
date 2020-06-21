02d0de59f104ffcce9b39ca9f3ae28999906f38e
leeyc924 <leeyc924@naver.com>
2020-06-21 14:34:55 +0900

    Top code

---
+++ src/java_20_06_21/Top.java
@@ -0,0 +1,19 @@
+package java_20_06_21;
+
+import java.util.*;
+
+public class Top {
+	public static void main(String[] args) {
+		int[] heights = { 1, 5, 3, 6, 7, 6, 5 };
+		int[] answer = new int[heights.length];
+		for (int i = 1; i < heights.length; i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (heights[i] < heights[j]) {
+					answer[i] = j + 1;
+					break;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+}
